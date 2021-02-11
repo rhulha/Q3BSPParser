671496ee9956854f1e470cea00db8172645f48b7
Ray <java4life@gmail.com>
2021-02-12 00:39:01 +0100

    Added a third mesh specifically for collision detection, after I learned that Octree building is quite expensive. So I removed all patches  that are pure eye candy ( cable and metalblack03 ). Also I removed the teleporter so one doesn't bump into it. I found that to be annoying. Also I removed the 8 sided jumppad so one doesn't bump into it. I found that to be annoying. (It was easier to simply remove all jump pads from the collision mesh, since with the triggers, one never touches the ground of them anyways.)

--- test/net/raysforge/gltf/TestBSP2glTF.java
+++ test/net/raysforge/gltf/TestBSP2glTF.java
@@ -23,6 +23,24 @@ public class TestBSP2glTF {
 		List<String> includeList = SpecialTexturesList.getQ3DM17BorderHighlightList();
 		bsp2glTF.addMeshFromBSP(includeList, true);
 		
+		// The next mesh is only for collision detection. So we remove the cable patches and some other stuff not needed, to make octreeing faster.
+		skipList = SpecialTexturesList.getSkipList();
+
+		skipList.add("textures/base_support/cable"); // the thin round cables, they can never be reached afaik.
+		skipList.add("textures/base_wall/metalblack03"); // the things above and below the thin round cables.
+
+		skipList.add("models/mapobjects/teleporter/energy");
+		skipList.add("models/mapobjects/teleporter/teleporter");
+		skipList.add("models/mapobjects/teleporter/teleporter_edge");
+		skipList.add("models/mapobjects/teleporter/pad");
+		skipList.add("models/mapobjects/teleporter/transparency");
+    	skipList.add("models/mapobjects/teleporter/widget");
+    	
+    	// the two 8 sided jump pads, they hinder the player a bit. So we remove them from the collision shape.
+    	skipList.add("textures/base_trim/border11light");
+		skipList.add("textures/sfx/diamond2cjumppad"); // if 8 vertices only ? well, since these are jump pads, we never need to collide with these anyways.
+		bsp2glTF.addMeshFromBSP(skipList, false);
+		
 		bsp2glTF.writeGLTF();
 		
 		
